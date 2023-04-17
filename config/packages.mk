d04395b2f415d7553305f661f72d7b263d23558b
darknius09 <darkniuslightness@gmail.com>
2023-04-17 04:54:56 +0000

    vendor: import more signal & wifi icon

--- config/packages.mk
+++ config/packages.mk
@@ -76,21 +76,59 @@ PRODUCT_PACKAGES += \
     HideCutout \
     StatusBarStock
 
-# Signal / WiFi Icons
+# Statusbar Icons
 PRODUCT_PACKAGES += \
-	BarsSignalOverlay \
-	BarsWiFiOverlay \
-	InsideSignalOverlay \
-	InsideWiFiOverlay \
-	RoundSignalOverlay \
-	RoundWiFiOverlay \
-	SneakySignalOverlay \
-	SneakyWiFiOverlay \
-	StrokeSignalOverlay \
-	StrokeWiFiOverlay \
-	WavySignalOverlay \
-	WavyWiFiOverlay \
-	XperiaSignalOverlay \
-	XperiaWiFiOverlay \
-	ZigZagSignalOverlay \
-	ZigZagWiFiOverlay
+    AcherusSignalOverlay \
+    DoraSignalOverlay \
+    StrokeSignalOverlay \
+    SneakySignalOverlay \
+    XperiaSignalOverlay \
+    ZigZagSignalOverlay \
+    WavySignalOverlay \
+    RoundSignalOverlay \
+    InsideSignalOverlay \
+    BarsSignalOverlay \
+    AquariumSignalOverlay \
+    ButterflySignalOverlay \
+    DaunSignalOverlay \
+    DecSignalOverlay \
+    DeepSignalOverlay \
+    EqualSignalOverlay \
+    FanSignalOverlay \
+    HuaweiSignalOverlay \
+    RelSignalOverlay \
+    ScrollSignalOverlay \
+    SeaSignalOverlay \
+    StackSignalOverlay \
+    WannuiSignalOverlay \
+    WindowsSignalOverlay \
+    WingSignalOverlay \
+    CircleSignalOverlay \
+    IosSignalOverlay \
+    MiniSignalOverlay \
+    OdinSignalOverlay \
+    PillsSignalOverlay \
+    RomanSignalOverlay  \
+    GradiconSignalOverlay \
+    NothingDotSignalOverlay \
+	OutlineSignalOverlay \
+	PUISignalOverlay 
+	
+
+# Wi-Fi Icons
+PRODUCT_PACKAGES += \
+    DoraWiFiOverlay \
+    AcherusWiFiOverlay \
+    StrokeWiFiOverlay \
+    SneakyWiFiOverlay \
+    XperiaWiFiOverlay \
+    ZigZagWiFiOverlay \
+    WavyWiFiOverlay \
+    WeedWiFiOverlay \
+    RoundWiFiOverlay \
+    InsideWiFiOverlay \
+    BarsWiFiOverlay \
+    GradiconWiFiOverlay \
+    OutlineWiFiOverlay \
+    NothingDotWiFiOverlay \
+    PavlovaWiFiOverlay
