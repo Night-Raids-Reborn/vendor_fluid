091f3e08bac57395ba04a3384457ae00642f37b6
Darknius Lighntess <darknius21@gmail.com>
2023-12-26 03:11:29 +0700

    common: remove spoof fp

--- config/common.mk
+++ config/common.mk
@@ -144,13 +144,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
-# Spoof fingerprint for Google Play Services and SafetyNet
-ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-else
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
-endif
-
 # Include Lawnchair
 ifeq ($(USE_LAWNCHAIR), true)
 $(call inherit-product, vendor/lawnchair/lawnchair.mk)
